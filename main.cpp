4a41cf842a289a61936fa801568e52866e93d6a0
Emil Missoni <pi.zero.w.em@gmail.com>
2021-10-10 22:08:58 +0200

    Update

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ int main(){
             {4, 5}
     };
     //std::cout << agentGenerator.get_size(sample_list);
-    sample_list = agentGenerator.edit(sample_list, 4, 6);
+    sample_list = agentGenerator.edit(sample_list, 2, 6);
     /*
     for (auto vector : sample_list){
         for (auto element : vector){
