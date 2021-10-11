94614cae58a2d7d60ddd427dda496522f174dd79
Emil Missoni <pi.zero.w.em@gmail.com>
2021-10-11 19:46:39 +0200

    Update

--- main.cpp
+++ main.cpp
@@ -14,29 +14,7 @@ void print_bigg_vector(std::vector<std::vector<std::vector<int>>> bigg_vector){
 
 int main(){
     AgentSystem agentSystem = AgentSystem();
-    std::vector<std::vector<std::vector<double>>> train_data = {
-            {
-                    {1, 0}, {1, 0}
-            },
-            {
-                    {1, 10}, {0, 1}
-            },
-            {
-                    {1, 20}, {0, 1}
-            },
-            {
-                    {3, 2}, {1, 0}
-            },
-            {
-                    {5, 4}, {1, 0}
-            },
-            {
-                    {1, 3}, {0, 1}
-            },
-            {
-                    {1, 1}, {0.5, 0.5}
-            },
-    };
-    Agent agent = agentSystem.train({2}, train_data, 0.01);
+
+    Agent agent = agentSystem.train({20, 15, 13, 8, 5, 4, 2}, train_data, 13.2);
     std::cout << agent.forward({6, 5})[0] << " " << agent.forward({6, 5})[1];
 }
