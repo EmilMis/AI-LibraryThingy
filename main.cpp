830005dd1c4c6bea15f81ce39823717747e73651
Emil Missoni <pi.zero.w.em@gmail.com>
2021-10-11 20:56:01 +0200

    Update

--- main.cpp
+++ main.cpp
@@ -1,16 +1,4 @@
 #include "Generator/Agent/AgentSystem.h"
-#include <iostream>
-
-void print_bigg_vector(std::vector<std::vector<std::vector<int>>> bigg_vector){
-    for (auto big_vector : bigg_vector){
-        for(auto vector : big_vector){
-            for (auto element : vector){
-                std::cout << element << " ";
-            }
-        }
-    }
-    std::cout << "\n";
-}
 
 int main(){
     AgentSystem agentSystem = AgentSystem();
