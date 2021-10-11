234fcb8500ddd2603841473dff98e76ec245008a
Emil Missoni <pi.zero.w.em@gmail.com>
2021-10-11 19:10:12 +0200

    Update

--- Generator/Agent/AgentSystem.h
+++ Generator/Agent/AgentSystem.h
@@ -10,7 +10,18 @@ Agent AgentSystem::train(std::vector<int> build, std::vector<std::vector<std::ve
     AgentGenerator agentGenerator = AgentGenerator();
     Generator generator = Generator();
     Agent agent = Agent(generator.generate_random_network(build, train_data[0][0].size()));
-    double best_fitness = agent.fitness();
+    double best_fitness = agent.full_fitness(train_data);
+    Agent best_agent = agent;
+    while (min_fitness < best_fitness){
+        std::vector<Agent> pop = agentGenerator.generate_agents(best_agent, (best_fitness - min_fitness) * 2.18);
+        for (auto agent_pop : pop){
+            if (agent_pop.full_fitness(train_data) < best_fitness){
+                best_fitness = agent_pop.full_fitness(train_data);
+                best_agent = agent_pop;
+            }
+        }
+    }
+    return best_agent;
 }
 
 AgentSystem::AgentSystem() = default;
--- main.cpp
+++ main.cpp
@@ -13,25 +13,5 @@ void print_bigg_vector(std::vector<std::vector<std::vector<int>>> bigg_vector){
 }
 
 int main(){
-    /*
-    AgentGenerator agentGenerator = AgentGenerator();
-    std::vector<std::vector<int>> generated = agentGenerator.generateIndexesToModify(15);
-    std::cout << "done";
-    for (auto big_vector : generated){
-        for (auto vector : big_vector){
-                std::cout << vector << " ";
-        }
-        std::cout << "\n";
-    }
-    std::cout << "\n";*/
-    Generator generator = Generator();
-    AgentGenerator agentGenerator = AgentGenerator();
-    Agent agent = Agent(generator.generate_random_network({1, 2}, 1));
-    std::vector<Agent> agets_pop = agentGenerator.generate_agents(agent, 1);
-    for (auto agent : agets_pop){
-        for (auto element : agent.forward({1})){
-            std::cout << element << " ";
-        }
-        std::cout << "\n";
-    }
+
 }
